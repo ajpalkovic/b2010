c574752f26b533c626745999482a1f786a608fae
AJ Palkovic <aj.palkovic@gmail.com>
2010-01-22 01:44:17 -0500

    Updated chainer player.

--- teams/team298/ChainerPlayer.java
+++ teams/team298/ChainerPlayer.java
@@ -11,10 +11,44 @@ public class ChainerPlayer extends AttackPlayer {
 
     }
 
-    public void boot() {
-        messaging.sendNewUnit();
-    }
-
     public void step() {
+        MapLocation location = sensing.senseClosestArchon();
+        int distance = location.distanceSquaredTo(controller.getLocation());
+
+        if(energon.isEnergonLow() || energon.isFluxFull() || distance > 34) {
+            navigation.changeToArchonGoal(true);
+            if(distance < 3) {
+                energon.transferFlux(location);
+                energon.requestEnergonTransfer();
+                controller.yield();
+            } else {
+                navigation.moveOnce(false);
+            }
+            return;
+        }
+
+        processEnemies();
+        sortEnemies();
+        EnemyInfo enemy = mode.getEnemyToAttack();
+        if(enemy != null) {
+            // attack
+            if(!controller.canAttackSquare(enemy.location)) {
+                navigation.faceLocation(enemy.location);
+                processEnemies();
+            }
+            executeAttack(enemy.location, enemy.type.isAirborne() ? RobotLevel.IN_AIR : RobotLevel.ON_GROUND);
+            processEnemies();
+            attackLocation = enemy.location;
+        } else {
+            if(outOfRangeEnemies.size() > 0) {
+                // only move if we can do it in 1 turn or less
+                if(controller.getRoundsUntilMovementIdle() < 2) {
+                    moveToAttack();
+                }
+            } else {
+                navigation.changeToMoveableDirectionGoal(true);
+                navigation.moveOnce(true);
+            }
+        }
     }
 }
