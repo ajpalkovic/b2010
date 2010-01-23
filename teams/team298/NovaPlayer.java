9d7bb476a665c15e7c81fb0610b701f0591ff5f8
AJ Palkovic <aj.palkovic@gmail.com>
2010-01-22 23:37:15 -0500

    Added delta sensing to wouts.

--- teams/team298/NovaPlayer.java
+++ teams/team298/NovaPlayer.java
@@ -110,6 +110,7 @@ public class NovaPlayer extends Base {
      */
     public void boot() {
         messaging.sendNewUnit();
+        sensing.senseAllTiles();
     }
 
     /**
--- teams/team298/WoutPlayer.java
+++ teams/team298/WoutPlayer.java
@@ -5,6 +5,9 @@ import static battlecode.common.GameConstants.*;
 import java.util.*;
 
 public class WoutPlayer extends NovaPlayer {
+    public int[] verticalDeltas = new int[] {-5, 0, -4, 3, -3, 4, -2, 4, -1, 4, 0, 5, 1, 4, 2, 4, 3, 4, 4, 3, 5, 0};
+    public int[] horizontalDeltas = new int[] {0, -5, 3, -4, 4, -3, 4, -2, 4, -1, 5, 0, 4, 1, 4, 2, 4, 3, 3, 4, 0, 5};
+    public int[] diagonalDeltas = new int[] {4, -3, 4, -2, 4, -1, 4, 0, 5, 0, 4, 1, 4, 2, 4, 3, 3, 3, 3, 4, 2, 4, 1, 4, 0, 4, 0, 5};
 
     public WoutPlayer(RobotController controller) {
         super(controller);
@@ -27,5 +30,9 @@ public class WoutPlayer extends NovaPlayer {
         } else {
             navigation.moveOnce(false);
         }
+    }
+
+    public void senseNewTiles() {
+        sensing.senseDeltas(verticalDeltas, horizontalDeltas, diagonalDeltas);
     }
 }
