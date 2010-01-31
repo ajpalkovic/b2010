90a4da063d1a0f08476ddc21f47568b5e89dc0e4
AJ Palkovic <aj.palkovic@gmail.com>
2010-01-31 14:42:09 -0500

    Updated script to run without the dialog.

--- battlecode.rb
+++ battlecode.rb
@@ -1,18 +1,13 @@
-require 'rubygems'
-
-maps = ENV['maps'].split(',')
-opponents = ENV['opponents'].split(',')
+ROOT = ENV['USERPROFILE']+"/"
 
 def forkAndRun(map, opponent)
   cmd = "#{File.join(FileUtils.pwd, 'ant.bat')} run > #{File.join(FileUtils.pwd, "#{map}-#{opponent}-output.txt")} &\n"
   output = system "#{cmd}"
+  sleep(3)
 end
 
-root = ENV['USERPROFILE']+"/"
-FileUtils.cp("#{root}.battlecode.ui", "#{root}.battlecode.ui.backup")
-for opponent in opponents
-  for map in maps
-      text = "#ui options
+def updateConfig(map, opponent)
+  text = "#ui options
 #Sun Jan 31 13:40:18 EST 2010
 file=
 analyzeFile=false
@@ -28,12 +23,39 @@ save-file=
 lastVersion=1.1.12
 host=
 lockstep=false"
-      File.open("#{root}.battlecode.ui", "w+") do |f|
-        f.write text
-      end
-      
-      forkAndRun(map, opponent)
-      sleep(3)
+  File.open("#{ROOT}.battlecode.ui", "w+") do |f|
+    f.write text
   end
 end
-FileUtils.cp("#{root}.battlecode.ui.backup", "#{root}.battlecode.ui")
+
+def run()
+  maps = ENV['maps'].split(',')
+  opponents = ENV['opponents'].split(',')
+  
+  if opponents.size > 1
+    for map in maps
+      for opponent in opponents
+          updateConfig(map, opponent)
+          forkAndRun(map, opponent)
+      end
+      puts "Press enter to move onto the next map."
+      gets unless map == maps[-1]
+    end
+  else
+    for opponent in opponents
+      for map in maps
+          updateConfig(map, opponent)
+          forkAndRun(map, opponent)
+      end
+      puts "Press enter to move onto the next opponent."
+      gets unless opponent == opponents[-1]
+    end
+  end
+end
+
+FileUtils.cp("#{ROOT}.battlecode.ui", "#{ROOT}.battlecode.ui.backup")
+FileUtils.cp(File.join(FileUtils.pwd, "bc.conf"), File.join(FileUtils.pwd, "bc.conf.backup"))
+FileUtils.cp(File.join(FileUtils.pwd, "bc.conf.noDialog"), File.join(FileUtils.pwd, "bc.conf"))
+run
+FileUtils.cp("#{ROOT}.battlecode.ui.backup", "#{ROOT}.battlecode.ui")
+FileUtils.cp(File.join(FileUtils.pwd, "bc.conf.backup"), File.join(FileUtils.pwd, "bc.conf"))
